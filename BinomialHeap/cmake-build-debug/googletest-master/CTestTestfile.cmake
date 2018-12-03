4a28e8a85d5f151cef00096c258bbfbf83a81923
akopianDA <230620ada>
2018-12-03 05:50:27 +0300

    First_Iteration

---
+++ BinomialHeap/cmake-build-debug/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/akopi/CLionProjects/BinomialHeap/googletest-master
+# Build directory: C:/Users/akopi/CLionProjects/BinomialHeap/cmake-build-debug/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
