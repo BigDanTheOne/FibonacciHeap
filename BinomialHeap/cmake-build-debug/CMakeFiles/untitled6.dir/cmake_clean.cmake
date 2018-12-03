68730dff9c2e7f8d4eaae6b658c0246455e358be
akopianDA <230620ada>
2018-12-03 05:33:03 +0300

    First_Iteration

---
+++ BinomialHeap/cmake-build-debug/CMakeFiles/untitled6.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled6.dir/main.cpp.obj"
+  "CMakeFiles/untitled6.dir/Tests/Test.cpp.obj"
+  "untitled6.pdb"
+  "untitled6.exe"
+  "untitled6.exe.manifest"
+  "libuntitled6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
